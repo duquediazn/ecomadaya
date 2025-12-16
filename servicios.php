8e0493678012b29b640743849bded3f9ddc72a08
duquediazn <duquediazn@gmail.com>
2025-12-16 20:32:49 +0000

    feat: añade contenido y estilizado a la página de servicios

---
+++ servicios.php
@@ -0,0 +1,43 @@
+<?php include 'includes/header.php'; ?>
+
+<section class="section--narrow">
+    <h1>Servicios de Tapicería y Restauración</h1>
+    <h2>Tapicería y restauración</h2>
+    <p>En <b>Madaya</b> recuperamos el estilo y la comodidad de tus muebles mediante técnicas artesanales y materiales de alta calidad.
+Tapizamos y restauramos <b>sofás, sillas, butacas, cabeceros y cualquier mueble</b> que necesite una nueva vida.</p>
+    <h3>¿Qué podemos hacer por ti?</h3>
+    <ul>
+        <li><b>Cambio de gomas y rellenos</b> para mayor confort.</li>
+        <li><b>Reparación y tratamiento de madera</b> para garantizar resistencia y durabilidad.</li>
+    </ul>
+    <p>Tras nuestro trabajo, tu mueble lucirá <b>como nuevo</b>, respetando su diseño original y mejorando su funcionalidad.</p>
+</section>
+
+<section class="sample-photos">
+    <h4>Tapicería general</h4>
+    <div class="picture-container">
+        <a href=""><img src="/assets/img/sofa1.webp" alt=""></a>
+        <a href=""><img src="/assets/img/butacas1.webp" alt=""></a>
+        <a href=""><img src="/assets/img/sillas1.webp" alt=""></a>
+    </div>
+    <h4>Restauración</h4>
+    <p>Restauración y tapizado de pequeña butaca clásica, antes y después.</p>
+    <div class="before-after-grid">
+        <div class="before-after-grid-col">
+            <p class="before-after__title">Antes</p>
+            <div class="picture-container">
+                <a href=""><img src="/assets/img/butaca-antes1.webp" alt=""></a>
+                <a href=""><img src="/assets/img/butaca-antes2.webp" alt=""></a>
+            </div>
+        </div>
+        <div class="before-after-grid-col">
+            <p class="before-after__title">Después</p>
+            <div class="picture-container">
+                <a href=""><img src="/assets/img/butaca-despues1.webp" alt=""></a>
+                <a href=""><img src="/assets/img/butaca-despues2.webp" alt=""></a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'includes/footer.php'; ?>
